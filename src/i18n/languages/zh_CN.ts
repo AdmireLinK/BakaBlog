1941fbd9d6ce91341dde084c29269632e839d157
AdmireLinK <admirezhong@gmail.com>
2025-09-25 15:26:24 +0800

    Update zh_CN.ts

--- src/i18n/languages/zh_CN.ts
+++ src/i18n/languages/zh_CN.ts
@@ -81,7 +81,7 @@ export const zh_CN: Translation = {
 	// 相册页面
 	[Key.albums]: "相册",
 	[Key.albumsSubtitle]: "记录生活中的美好瞬间",
-	[Key.albumsEmpty]: "暂无相册",
+	[Key.albumsEmpty]: "暂无",
 	[Key.albumsEmptyDesc]: "还没有创建任何相册，快去添加一些美好的回忆吧！",
 	[Key.albumsBackToList]: "返回相册",
 	[Key.albumsPhotoCount]: "张照片",
