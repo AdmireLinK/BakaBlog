bfbaf8a2442e21c15ac004cb693cafad684f2480
AdmireLinK <64939678+AdmireLinK@users.noreply.github.com>
2025-09-26 11:05:39 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -43,9 +43,9 @@ export const siteConfig: SiteConfig = {
 	bangumi: {
 		userId: "cosmosuperbaka", // 在此处设置你的Bangumi用户ID，可以设置为 "sai" 测试
 	},
-  
+
 	anime: {
-		mode: "local", // 番剧页面模式："bangumi" 使用Bangumi API，"local" 使用本地配置
+		mode: "bangumi", // 番剧页面模式："bangumi" 使用Bangumi API，"local" 使用本地配置
 	},
 
 	banner: {
